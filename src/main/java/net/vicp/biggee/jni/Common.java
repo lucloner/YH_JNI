02fd5411931085025031cd64800e609c596cbf07
lucloner <3920443+lucloner@users.noreply.github.com>
2019-12-02 19:18:30 +0800

    还缺修正算法 2

---
+++ src/main/java/net/vicp/biggee/jni/Common.java
@@ -0,0 +1,13 @@
+package net.vicp.biggee.jni;
+
+/**
+ * @program: YH_JNI
+ * @description:
+ * @author: Biggee
+ * @create: 2019-12-02 18:00
+ **/
+public class Common {
+    public static void main(String[] args) {
+        
+    }
+}
